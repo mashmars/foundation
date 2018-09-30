6d9989666e46ab71634d267999fa9b3814c43ae4
mashmars <402738923@qq.com>
2018-09-30 14:07:45 +0800

    d

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation()
+/*菜单下行线 使用方式 div增加.hover-underline-menu 以及属性 data-menu-underline-from-center*/
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
+/**查询切换 */
+$('.search').click(function () {
+    $('#search').toggle();
+})
+
